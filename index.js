1c4401a8a4ac7c6979997dd6af80c7f118a5cd8f
Miguel_Reis <guelsouzar@gmail.com>
2022-03-14 17:59:07 -0300

    req07

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const { validDisplayName, validEmail, validPassword, verifyToken } = require('./
 const { validEmailLogin, validPasswordLogin } = require('./middleware/Login');
 const { validName } = require('./middleware/Categories');
 const { validCategoryIds, validTitle, validContent } = require('./middleware/BlogPost');
+const { createBlogPosts } = require('./controller/BlogPosts');
 
 const app = express();
 app.use(bodyParser.json());
 const Users = require('./controller/Users');
 const Login = require('./controller/Login');
 const Categories = require('./controller/Categories');
-const BlogPosts = require('./controller/BlogPosts');
 
 app.listen(3000, () => console.log('ouvindo porta 3000!'));
 
@@ -20,7 +20,7 @@ app.get('/user', verifyToken, Users.everthingUsers);
 app.get('/user/:id', verifyToken, Users.everthingIdUsers);
 app.post('/categories', validName, verifyToken, Categories.createCategories);
 app.get('/categories', verifyToken, Categories.everthingCategories);
-app.post('/post', verifyToken, validCategoryIds, validTitle, validContent, BlogPosts.createBlogPosts);
+app.post('/post', verifyToken, validCategoryIds, validTitle, validContent, createBlogPosts);
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (request, response) => {
